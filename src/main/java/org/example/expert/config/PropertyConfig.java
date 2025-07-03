6255e60a1700c9d90fc6be8b6c142a82d6a0e57a
huouvcti <huouvcti@gamil.com>
2025-07-03 13:58:52 +0900

    env 설정 방식 변경
    - env.properties, config 방식 -> .env, 구성 편집 > 환경변수 추가

--- src/main/java/org/example/expert/config/PropertyConfig.java
+++
@@ -1,9 +0,0 @@
-package org.example.expert.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-
-@Configuration
-@PropertySource("classpath:properties/env.properties") // env.properties 파일 소스 등록
-public class PropertyConfig {
-}
